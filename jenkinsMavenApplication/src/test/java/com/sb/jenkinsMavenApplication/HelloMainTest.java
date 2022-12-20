bc14a2adf2b2da56b72a1cc94aa65e9d5b484bfd
suramya-biswas-git <suramya.biswas@gmail.com>
2022-12-20 16:25:48 +0530

    First Version

---
+++ jenkinsMavenApplication/src/test/java/com/sb/jenkinsMavenApplication/HelloMainTest.java
@@ -0,0 +1,20 @@
+package com.sb.jenkinsMavenApplication;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class HelloMainTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
