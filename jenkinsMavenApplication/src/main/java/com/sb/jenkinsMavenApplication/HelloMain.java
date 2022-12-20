a5f8cf649377244d77f3554d0b9fc6840c1759b4
suramya-biswas-git <suramya.biswas@gmail.com>
2022-12-20 17:45:38 +0530

    Third Version

--- jenkinsMavenApplication/src/main/java/com/sb/jenkinsMavenApplication/HelloMain.java
+++ jenkinsMavenApplication/src/main/java/com/sb/jenkinsMavenApplication/HelloMain.java
@@ -2,5 +2,6 @@ package com.sb.jenkinsMavenApplication;
 public class HelloMain {
     public static void main( String[] args ){
         System.out.println( "Hello World!" );
+        System.out.println( "Hello Jenkins!" );
     }
 }
